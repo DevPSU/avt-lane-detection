1a59aad69c86d93eb405ac1456dd8d17adbcea37
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2019-09-16 18:45:50 -0400

    Update perception.py

--- perception.py
+++ perception.py
@@ -14,6 +14,6 @@ def canny(image):
 
 
 # Show image
-cv2.imshow('image', canny('Advanced-Vehicle-Technology/talos-nd01 copy/1568087426_1388_-9.600000000000001_12.28v_30.0c_frontlower.jpg'))
+cv2.imshow('image', canny('avt-lane-detection/talos-nd01 copy/1568087426_1388_-9.600000000000001_12.28v_30.0c_frontlower.jpg'))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
