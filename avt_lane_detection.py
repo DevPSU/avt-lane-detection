00a0572e8053823b51feb45557e0d7d6a7369603
liketoast <43727208+LiZhu912@users.noreply.github.com>
2019-10-10 21:14:27 -0400

    Added mid point
    Midpoint is calculated by applying the midpoint formula to the bottom point of the left lane and right lane line.

--- avt_lane_detection.py
+++ avt_lane_detection.py
@@ -62,17 +62,18 @@ def display_lines(image, lines):
     return line_image
 
 # Working on some sort of median line
-##def midpoint(image, lines):
-##    x1, y1, x2, y2 = lines[0]
-##    x3, y3, x4, y4 = lines[1]
-##    
-##    xa = int((x3 - x1)/2)
-##    ya = int((y3 - y1)/2)
-##
-##    xb = int((x4 - x2)/2)
-##    yb = int((y4 - y2)/2)
-##
-##    return (xa, ya), (xb, yb)
+def midpoint(image, average_lines):
+    left_point = (average_lines[0][0],average_lines[0][1])
+    right_point = (average_lines[1][0],average_lines[1][1])
+
+    # Calculate the midpoint
+    midpoint_x = int((right_point[0]-left_point[0])/2 + left_point[0])
+    midpoint_y = 720
+    midpoint = (midpoint_x, midpoint_y)
+
+    # Draw the midpoint
+    cv2.circle(image, midpoint, 10, (0,255,0), -1)
+    return image
 
 # carsample.jpg is a temporary image from the data set
 lane_image = cv2.imread("carsample.jpg")
@@ -90,14 +91,15 @@ averaged_lines = average_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, averaged_lines)
 
 # combo_image combines the line_image and lane_image
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+lane_with_lines_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+lane_with_lines_image = cv2.addWeighted(lane_with_lines_image, 0.8, midpoint(lane_image, averaged_lines), 1, 1)
 
 # plt helps show the image in a graph
 #plt.imshow(lane_image)
 #plt.show()
 
 # display image
-cv2.imshow("image", combo_image)
+cv2.imshow("image", lane_with_lines_image)
 cv2.waitKey(0)
 
 
